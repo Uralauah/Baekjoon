852bf7bdedca435a68f6e7a492d5752bd416d86d
LeeSeunghyun <50231309+Uralauah@users.noreply.github.com>
2023-08-07 14:10:12 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+	int world[1000][3];
+	int n, k;
+	cin >> n >> k;
+	int num, g, s, b;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num >> g >> s >> b;
+		world[num][0] = g;
+		world[num][1] = s;
+		world[num][2] = b;
+	}
+	int score=1;
+	if (world[k][0] > 0)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (world[i][0] == world[k][0])
+			{
+				if (world[i][1] == world[k][1])
+				{
+					if (world[i][2] > world[k][2])
+						score++;
+				}
+				else if (world[i][1] > world[k][1])
+					score++;
+
+			}
+			else if (world[i][0] > world[k][0])
+				score++;
+		}
+	}
+	else if (world[k][0] == 0 && world[k][1] > 0)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (world[i][0] > 0)
+				score++;
+			else if (world[i][0] == 0 && world[i][1] == world[k][1])
+			{
+				if (world[i][2] > world[i][2])
+					score++;
+			}
+			else if (world[i][0] == 0 && world[i][1] > world[k][1])
+				score++;
+		}
+	}
+	else if (world[k][0] == 0 && world[k][1] == 0 && world[k][2] > 0)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (world[i][0] > 0)
+				score++;
+			else if (world[i][1] > 0)
+				score++;
+			else if (world[i][2] > world[k][2])
+				score++;
+		}
+	}
+	cout << score;
+}
