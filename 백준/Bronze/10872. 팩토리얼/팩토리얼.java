d3f8505f185088ef538b563b4bb7057a305e8ee5
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2023-08-17 15:09:07 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int sum=1;
+        for (int i=1;i<=n;i++)
+            sum*=i;
+        System.out.println(sum);
+    }
+}
