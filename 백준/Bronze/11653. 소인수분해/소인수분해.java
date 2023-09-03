fc0f8c0e6ae7232ab987160d2ab133ecff3ef361
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2023-09-03 19:58:44 +0900

    [Bronze I] Title: 소인수분해, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,34 +1,19 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int max = 10000000;
-        boolean [] prime = new boolean[max+1];
-        Arrays.fill(prime,true);
-        prime[0] = prime[1] = false;
-        for (int i=2;i*i<=max;i++){
-            if (prime[i]) {
-                for (int j = i * i; j <= max; j += i) {
-                    prime[j] = false;
-                }
-            }
-        }
-
         int n = Integer.parseInt(br.readLine());
-        while(n!=1){
-            for (int i=2;i<=n;i++){
-                if (prime[i]&&n%i==0){
-                    System.out.println(i);
-                    n/=i;
-                    break;
-                }
+        for (int i=2;i<=Math.sqrt(n);i++){
+            while(n%i==0){
+                System.out.println(i);
+                n/=i;
             }
         }
-
+        if (n!=1)
+            System.out.println(n);
     }
 }
