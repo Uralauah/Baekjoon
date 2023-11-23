4f1f89777c5a0930a949bcf4a906440240415622
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2023-11-23 12:46:36 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 632 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    int t;
+    int a,b;
+    cin>>t;
+    for (long long int i=0;i<t;i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<'\n';
+    }
+}
