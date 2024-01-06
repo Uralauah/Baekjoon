8e212d6d436f544c07e695593803a7e23adef801
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-01-06 16:59:29 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/\n"
+            "      |\n"
+            "      |";
+}
