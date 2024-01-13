ab24f6423aa22b06d5ba1c116ac22df211422b82
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-01-13 13:42:10 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int Fibo(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << Fibo(n);
+}
