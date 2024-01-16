b6adc2a5977fb8cb59f40cc7b215e9f6a1677170
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-01-16 21:13:12 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout<<".  .   .\n"
+          "|  | _ | _. _ ._ _  _\n"
+          "|/\\|(/.|(_.(_)[ | )(/.";
+    
+}
