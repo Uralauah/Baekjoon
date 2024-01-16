ac8d46993e8570bbd9e11248f0f485644ec0be1d
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-01-16 20:34:27 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,18 +2,23 @@
 #include <cmath>
 #include <string>
 
+#define M 1234567891
+
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     string input;
     int l, n;
-    long long sum = 0;
+    long long sum = 0, r = 1;
     cin >> l >> input;
 
     for (int i = 0; i < l; i++) {
         n = input[i] - 'a' + 1;
-        
-        sum += n * pow(31, i);
+
+        sum = (sum + n * r) % M;
+        r = r * 31 % M;
     }
     cout << sum % 1234567891;
 }
