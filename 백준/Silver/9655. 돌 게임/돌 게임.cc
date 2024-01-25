e8af23ea3d4fdc400b468e7e4da381bfb6751bfc
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-01-25 13:56:41 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    if (!(n % 2)) {
+        cout << "CY";
+    } else
+        cout << "SK";
+}
