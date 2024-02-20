037f799f0aba77a568bee9f3d22b1836716125ac
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-02-20 17:42:51 +0900

    [Gold IV] Title: 인구 이동, Time: 96 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 #include <queue>
-#include <set>
 
 using namespace std;
-
-int n, l, r, ans = 0;
+vector<pair<int, int>> v;
+queue<pair<int, int>> q;
+int n, l, r, sum = 0;
 int city[51][51], dir[4][2] = {1, 0, -1, 0, 0, 1, 0, -1};
 bool visited[51][51];
-queue<pair<int, int>> q, q2, ali;
-set<pair<int, int>> s;
 
 bool check(int x, int y) {
     if (x < 1 || x > n || y < 1 || y > n)
@@ -17,82 +15,69 @@ bool check(int x, int y) {
     return true;
 }
 
-void find(int x, int y) {
-    q.push({x, y});
-    ali.push({x, y});
-    int people = city[x][y];
-    visited[x][y] = true;
-    //cout << "xy       " << x << " " << y << endl;
+void find(pair<int, int> input) {
+    q.push(input);
+    visited[input.first][input.second] = true;
     while (!q.empty()) {
-        x = q.front().first;
-        y = q.front().second;
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
         for (int i = 0; i < 4; i++) {
             int tx = x + dir[i][0];
             int ty = y + dir[i][1];
-            if (check(tx, ty)) {
-                if (!visited[tx][ty]) {
-                    int sum = abs(city[x][y] - city[tx][ty]);
-                    if (sum >= l && sum <= r) {
-                        ali.push({tx, ty});
+            if (!visited[tx][ty]) {
+                if (check(tx, ty)) {
+                    int temp = abs(city[x][y] - city[tx][ty]);
+                    if (temp >= l && temp <= r) {
+                        v.push_back({tx, ty});
                         q.push({tx, ty});
                         visited[tx][ty] = true;
-                        people += city[tx][ty];
-                    } else {
-                        s.insert({tx, ty});
+                        sum += city[tx][ty];
                     }
                 }
+
             }
         }
     }
-
-    people = people / ali.size();
-//    cout << people << endl;
-    while (!ali.empty()) {
-//        cout << ali.front().first << " " << ali.front().second << endl;
-        city[ali.front().first][ali.front().second] = people;
-        ali.pop();
-    }
-    if (!s.empty()) {
-        ans++;
-        while (true) {
-            if (s.empty())
-                break;
-            x = (*s.begin()).first;
-            y = (*s.begin()).second;
-            s.erase(s.begin());
-            if (!visited[x][y])
-                break;
-        }
-        find(x, y);
-    }
 }
 
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n >> l >> r;
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+        for (int j = 1; j <= n; j++)
             cin >> city[i][j];
-        }
     }
-
-    int sum = 0;
-    while (true) {
-        sum++;
-        find(1, 1);
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 1; j <= n; j++)
-//                cout << city[i][j] << " ";
-//            cout << endl;
-//        }
-//        cout << ans << endl;
+    bool flag = true;
+    int ans = 0;
+    while (flag) {
+        flag = false;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (!visited[i][j]) {
+                    v.push_back({i, j});
+                    sum = city[i][j];
+                    find({i, j});
+                }
+                if (v.size() > 1) {
+                    flag = true;
+                    for (int k = 0; k < v.size(); k++) {
+                        city[v[k].first][v[k].second] = sum / v.size();
+                    }
+                }
+                v.clear();
+            }
+        }
+        if (flag)
+            ans++;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++)
                 visited[i][j] = false;
         }
-        if (ans == n * n - 1)
-            break;
-        ans = 0;
+
     }
-    cout << sum - 1;
+    cout << ans;
 }
