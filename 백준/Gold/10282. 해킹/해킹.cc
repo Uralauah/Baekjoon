fa7fa999d9ea534c0c6db00ca9bfbb2a0c9af32f
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-02-24 17:14:41 +0900

    [Gold IV] Title: 해킹, Time: 128 ms, Memory: 4184 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.cc
+++ 백준/Gold/10282. 해킹/해킹.cc
@@ -1,72 +1,61 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <queue>
 
-
-#define MAX (10001)
-#define INF (987654321)
+#define INF 987654321
 
 using namespace std;
 
-int T, N, D, C;
-int dp[MAX];
 
-void dijkstra(vector<pair<int, int>> edge[]) {
-	dp[C] = 0;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.push({ 0,C });
+int n, d, c, total = 0, ans = 0;
 
-	while (!pq.empty()) {
-		int cur = pq.top().second;
-		int curTime = pq.top().first;
-		pq.pop();
-		if (dp[cur] < curTime)
-			continue;
+int arr[10001];
 
-		for (int i = 0; i < edge[cur].size(); i++) {
-			int next = edge[cur][i].first;
-			int nextTime = curTime + edge[cur][i].second;
+void find(vector<pair<int, int>> v[]) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, c});
+    arr[c] = 0;
+    while (!pq.empty()) {
+        int node = pq.top().second;
+        int sec = pq.top().first;
+        pq.pop();
+        if (arr[node] < sec)
+            continue;
+        for (int i = 0; i < v[node].size(); i++) {
+            int next = v[node][i].second;
+            int temp = v[node][i].first + sec;
 
-			if (nextTime < dp[next]) {
-				dp[next] = nextTime;
-				pq.push({ nextTime, next });
-			}
-
-		}
-	}
-	//INF가 아닌 수 중에서 가장 큰 값이 모든 PC 감염 시간
-	//INF가 아닌 수들의 개수가 감염된 컴퓨터의 수
-	int cnt=0,ans = 0;
-	for (int i = 1; i <= N; i++) {
-		if (dp[i] != INF) {
-			cnt++;
-			ans = max(ans, dp[i]);
-		}
-	}
-	cout << cnt << ' ' << ans<< '\n';
-
+            if (arr[next] > temp) {
+                arr[next] = temp;
+                pq.push({temp, next});
+            }
+        }
+    }
+    for (int j = 1; j <= n; j++) {
+        if (arr[j] != INF) {
+            total++;
+            ans = max(ans, arr[j]);
+        }
+    }
+    cout << total << " " << ans << '\n';
 }
 
 int main() {
-
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	cin >> T;
-	while (T--) {
-		vector<pair<int, int>> edge[MAX];
-		cin >> N >> D >> C;
-		int a, b, s;
-		for (int i = 0; i < D; i++) {
-			cin >> a >> b >> s;
-			//b가 감염되면 a는 s초 후에 감염
-			edge[b].push_back({ a,s });
-		}
-		fill_n(dp, MAX, INF);
-		dijkstra(edge);
-
-	}
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    int a, b, s;
+    for (int i = 0; i < t; i++) {
+        vector<pair<int, int>> v[10001];
+        cin >> n >> d >> c;
+        for (int j = 1; j <= d; j++) {
+            cin >> a >> b >> s;
+            v[b].push_back({s, a});
+        }
+        fill_n(arr, 10001, INF);
+        find(v);
+        total = 0, ans = 0;
+    }
 }
