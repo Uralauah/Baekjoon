b648ce78726caa274741ca05ab6aade407f895b5
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-04-22 18:34:07 +0900

    [level 2] Title: 전화번호 목록, Time: 304.84 ms, Memory: 187 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -7,13 +7,27 @@ class Solution {
             hs.add(phone_book[i]);
         }
         
-        for(int i=0;i<phone_book.length;i++){
-            for(int j=1;j<phone_book[i].length();j++){
-                if(hs.contains(phone_book[i].substring(0,j))){
+        for(String tmp : hs){
+            for(int i = 1; i<tmp.length();i++){
+                if(hs.contains(tmp.substring(0,i)))
                     return false;
-                }
             }
         }
+        
         return answer;
+//         boolean answer = true;
+//         HashSet<String> hs = new HashSet<>();
+//         for(int i=0;i<phone_book.length;i++){
+//             hs.add(phone_book[i]);
+//         }
+        
+//         for(int i=0;i<phone_book.length;i++){
+//             for(int j=1;j<phone_book[i].length();j++){
+//                 if(hs.contains(phone_book[i].substring(0,j))){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return answer;
     }
 }
