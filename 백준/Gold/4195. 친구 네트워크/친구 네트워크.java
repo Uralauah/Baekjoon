3d873eaa209397e06a385eb7ea2c561006b1264b
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-07-09 20:26:57 +0900

    [Gold II] Title: 친구 네트워크, Time: 1484 ms, Memory: 83500 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.Provider;
+import java.util.*;
+
+public class Main {
+    public static int N, F;
+    public static HashMap<String, String> network;
+    public static HashMap<String,Integer> friendNum;
+
+    public static String find(String name){
+        if(!network.get(name).equals(name)){
+            network.put(name,find(network.get(name)));
+        }
+        return network.get(name);
+    }
+
+    public static void union(String n1, String n2){
+        String p1 = find(n1);
+        String p2 = find(n2);
+        int num1 = friendNum.get(p1);
+        int num2 = friendNum.get(p2);
+        if(!p1.equals(p2)){
+            network.put(p2,p1);
+            friendNum.put(p1,num1+num2);
+        }
+    }
+
+    public static int getFriendNum(String name){
+        String parent = find(name);
+        return friendNum.get(parent);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        network = new HashMap<>();
+        friendNum = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            F = Integer.parseInt(br.readLine());
+            network.clear();
+            friendNum.clear();
+            for (int j = 0; j < F; j++) {
+                String n1, n2;
+                int num;
+                st = new StringTokenizer(br.readLine());
+                n1 = st.nextToken();
+                n2 = st.nextToken();
+
+                if(!network.containsKey(n1))
+                    network.put(n1,n1);
+
+                if(!network.containsKey(n2))
+                    network.put(n2, n2);
+
+                if(!friendNum.containsKey(n1)){
+                    friendNum.put(n1,1);
+                }
+
+                if(!friendNum.containsKey(n2)){
+                    friendNum.put(n2, 1);
+                }
+
+                String p1 = find(n1);
+                String p2 = find(n2);
+
+                if(p1.equals(p2)) {
+                    System.out.println(getFriendNum(n1));
+                    continue;
+                }
+                union(n1,n2);
+                System.out.println(getFriendNum(n1));
+            }
+        }
+    }
+}
