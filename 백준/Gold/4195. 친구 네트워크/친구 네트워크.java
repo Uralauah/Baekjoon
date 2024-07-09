be1f2cf7ce25f381774603994902c2feea6374bf
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-07-09 20:28:05 +0900

    [Gold II] Title: 친구 네트워크, Time: 892 ms, Memory: 72608 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -7,27 +7,27 @@ import java.util.*;
 public class Main {
     public static int N, F;
     public static HashMap<String, String> network;
-    public static HashMap<String,Integer> friendNum;
+    public static HashMap<String, Integer> friendNum;
 
-    public static String find(String name){
-        if(!network.get(name).equals(name)){
-            network.put(name,find(network.get(name)));
+    public static String find(String name) {
+        if (!network.get(name).equals(name)) {
+            network.put(name, find(network.get(name)));
         }
         return network.get(name);
     }
 
-    public static void union(String n1, String n2){
+    public static void union(String n1, String n2) {
         String p1 = find(n1);
         String p2 = find(n2);
         int num1 = friendNum.get(p1);
         int num2 = friendNum.get(p2);
-        if(!p1.equals(p2)){
-            network.put(p2,p1);
-            friendNum.put(p1,num1+num2);
+        if (!p1.equals(p2)) {
+            network.put(p2, p1);
+            friendNum.put(p1, num1 + num2);
         }
     }
 
-    public static int getFriendNum(String name){
+    public static int getFriendNum(String name) {
         String parent = find(name);
         return friendNum.get(parent);
     }
@@ -35,6 +35,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
         N = Integer.parseInt(br.readLine());
         network = new HashMap<>();
         friendNum = new HashMap<>();
@@ -42,37 +43,32 @@ public class Main {
             F = Integer.parseInt(br.readLine());
             network.clear();
             friendNum.clear();
+
             for (int j = 0; j < F; j++) {
                 String n1, n2;
-                int num;
                 st = new StringTokenizer(br.readLine());
                 n1 = st.nextToken();
                 n2 = st.nextToken();
 
-                if(!network.containsKey(n1))
-                    network.put(n1,n1);
-
-                if(!network.containsKey(n2))
-                    network.put(n2, n2);
-
-                if(!friendNum.containsKey(n1)){
-                    friendNum.put(n1,1);
+                if (!network.containsKey(n1)) {
+                    network.put(n1, n1);
+                    friendNum.put(n1, 1);
                 }
 
-                if(!friendNum.containsKey(n2)){
+                if (!network.containsKey(n2)) {
+                    network.put(n2, n2);
                     friendNum.put(n2, 1);
                 }
 
                 String p1 = find(n1);
                 String p2 = find(n2);
 
-                if(p1.equals(p2)) {
-                    System.out.println(getFriendNum(n1));
-                    continue;
+                if (!p1.equals(p2)) {
+                    union(n1, n2);
                 }
-                union(n1,n2);
-                System.out.println(getFriendNum(n1));
+                sb.append(getFriendNum(n1)).append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
